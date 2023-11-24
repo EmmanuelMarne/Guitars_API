e0c0b2ad650c1f849800858bd83fb4103a8ce67e
Emmanuel <manumtz20@gmail.com>
2023-11-24 17:31:55 -0600

    nueva ruta

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,8 @@ routers.get("/", (req, res) => {
   res.send("Hi, welcome again to server's guitars");
 });
 
+routers.get("/saludar", (req, res) => {
+  res.send("otro saludo");
+});
+
 module.exports = routers;
