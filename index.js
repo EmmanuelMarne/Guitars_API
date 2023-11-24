0a28c721544f2ab7094b2e4d097deaa3220e26bb
Emmanuel <manumtz20@gmail.com>
2023-11-24 14:49:06 -0600

    primer commit

---
+++ index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const server = require("./src/server");
+
+const { PORT } = process.env;
+
+server.listen(PORT, () => {
+  console.log(`server listen in port: ${PORT}`);
+});
