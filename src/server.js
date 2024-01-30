49cea4d15e7ea5d5f0d43d0cb99ecba698e21139
Emmanuel <manumtz20@gmail.com>
2024-01-30 13:59:55 -0600

    nuevo formateo

--- src/server.js
+++ src/server.js
@@ -9,17 +9,10 @@ server.disable('x-powered-by')
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
-
-// server.get("/", (req, res) => {
-//   res.send("Hi, welcome to server's guitars");
-// });
 
 server.use(routers);
 
